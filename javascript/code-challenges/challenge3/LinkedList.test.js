049820156f200a122e981c7ea08a5266aa334203
Nour-Alahmad <Nour.Alahmad1@outlook.com>
2023-06-21 20:58:53 +0000

    done

--- javascript/code-challenges/challenge3/LinkedList.test.js
+++ javascript/code-challenges/challenge3/LinkedList.test.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 class Node {
   constructor(value) {
@@ -9,7 +9,6 @@ class Node {
 
 class LinkedList {
   constructor() {
-
     this.head = null;
     this.tail = null;
   }
@@ -26,7 +25,7 @@ class LinkedList {
     }
   }
 
-  append(value){
+  append(value) {
     const newNode = new Node(value);
     if (!this.head) {
       this.head = newNode;
@@ -43,12 +42,12 @@ class LinkedList {
     this.tail.next = null;
     // console.log(this);
   }
-  insertBefore(value, newValue){
+  insertBefore(value, newValue) {
     let currentNode = this.head;
-    if(currentNode.value === value || currentNode === null){
+    if (currentNode.value === value || currentNode === null) {
       this.append(newValue);
     }
-    while( currentNode.next !== null && currentNode.next.value !== value){
+    while (currentNode.next !== null && currentNode.next.value !== value) {
       currentNode = currentNode.next;
     }
     const newNode = new Node(newValue);
@@ -56,10 +55,10 @@ class LinkedList {
     currentNode.next = newNode;
   }
 
-  insertAfter(value, newValue){
+  insertAfter(value, newValue) {
     let currentNode = this.head;
 
-    while(currentNode !== null && currentNode.value !== value){
+    while (currentNode !== null && currentNode.value !== value) {
       currentNode = currentNode.next;
     }
     const newNode = new Node(newValue);
@@ -77,6 +76,41 @@ class LinkedList {
     return false;
   }
 
+  findNodeAtIndex(index) {
+    let currentNode = this.head;
+    let count = 0;
+    while (currentNode) {
+      if (count === index) {
+        return currentNode.value;
+      }
+      count++;
+      currentNode = currentNode.next;
+    }
+    return "not found";
+  }
+
+  findLength() {
+    let count = 0;
+    let currentnode = this.head;
+    while (currentnode !== null) {
+      count++;
+      currentnode = currentnode.next;
+    }
+    return count;
+  }
+
+  kthNodeFromTail(k) {
+    // console.log('this is the list', this, 'and this is the k',
+    //   k );
+    if (this.length < k || k === 0 || k < 0) {
+      return "k must be greater than or equal to The length of your linkedlist";
+    }
+    const length = this.findLength();
+    const kFromHead = length - k;
+    console.log(kFromHead);
+    return this.findNodeAtIndex(kFromHead);
+  }
+
   tostring() {
     let currentNode = this.head;
     let str = "";
@@ -87,7 +121,6 @@ class LinkedList {
     return str + currentNode.value + " >> NULL";
   }
 }
-
 
 let newList = new LinkedList();
 
@@ -100,45 +133,46 @@ newList.insert(5);
 const newList2 = new LinkedList();
 
 newList2.append(1);
-newList2.append(2);
-newList2.append(3);
-newList2.append(4);
-newList2.append(5);
+// newList2.append(2);
+// newList2.append(3);
+// newList2.append(4);
+// newList2.append(5);
 
-console.log(newList.tostring());
 console.log(newList2.tostring());
 
+console.log(newList2.kthNodeFromTail(-1));
 
+// console.log(newList2.tostring());
 
 //------------------------------------------------------------------
 
 //test
 
-describe('Linked List', () => {
+describe("Linked List", () => {
   let linkedList;
 
   beforeEach(() => {
     linkedList = new LinkedList();
   });
 
-  it('should instantiate an empty linked list', () => {
+  it("should instantiate an empty linked list", () => {
     expect(linkedList.head).toBeNull();
   });
 
-  it('should properly insert into the linked list', () => {
+  it("should properly insert into the linked list", () => {
     linkedList.insert(5);
     expect(linkedList.head.value).toBe(5);
     expect(linkedList.head.next).toBeNull();
   });
 
-  it('should have the head property properly point to the first node', () => {
+  it("should have the head property properly point to the first node", () => {
     linkedList.insert(5);
     linkedList.insert(10);
     expect(linkedList.head.value).toBe(10);
     expect(linkedList.head.next.value).toBe(5);
   });
 
-  it('should properly insert multiple nodes into the linked list', () => {
+  it("should properly insert multiple nodes into the linked list", () => {
     linkedList.insert(5);
     linkedList.insert(10);
     linkedList.insert(15);
@@ -147,67 +181,112 @@ describe('Linked List', () => {
     expect(linkedList.head.next.next.value).toBe(5);
   });
 
-  it('should return true when finding an existing value', () => {
+  it("should return true when finding an existing value", () => {
     linkedList.insert(5);
     linkedList.insert(10);
     linkedList.insert(15);
     expect(linkedList.includes(10)).toBe(true);
   });
 
-  it('should return false when finding a non-existing value', () => {
+  it("should return false when finding a non-existing value", () => {
     linkedList.insert(5);
     linkedList.insert(10);
     linkedList.insert(15);
     expect(linkedList.includes(20)).toBe(false);
   });
 
-  it('should return a collection of all values in the linked list', () => {
+  it("should return a collection of all values in the linked list", () => {
     linkedList.insert(5);
     linkedList.insert(10);
     linkedList.insert(15);
     const values = linkedList.tostring();
-    expect(values).toBe('15 >> 10 >> 5 >> NULL');
+    expect(values).toBe("15 >> 10 >> 5 >> NULL");
   });
-  it('should successfully add a node to the end of the linked list', () => {
+  it("should successfully add a node to the end of the linked list", () => {
     linkedList.append(1);
-    expect(linkedList.tostring()).toBe('1 >> NULL');
+    expect(linkedList.tostring()).toBe("1 >> NULL");
   });
 
-  it('should successfully add multiple nodes to the end of a linked list', () => {
+  it("should successfully add multiple nodes to the end of a linked list", () => {
     linkedList.append(1);
     linkedList.append(2);
     linkedList.append(3);
-    expect(linkedList.tostring()).toBe('1 >> 2 >> 3 >> NULL');
+    expect(linkedList.tostring()).toBe("1 >> 2 >> 3 >> NULL");
   });
 
-  it('should successfully insert a node before a node located in the middle of a linked list', () => {
+  it("should successfully insert a node before a node located in the middle of a linked list", () => {
     linkedList.append(1);
     linkedList.append(2);
     linkedList.append(4);
     linkedList.append(5);
     linkedList.insertBefore(4, 3);
-    expect(linkedList.tostring()).toBe('1 >> 2 >> 3 >> 4 >> 5 >> NULL');
+    expect(linkedList.tostring()).toBe("1 >> 2 >> 3 >> 4 >> 5 >> NULL");
   });
 
-  it('should successfully insert a node before the first node of a linked list', () => {
+  it("should successfully insert a node before the first node of a linked list", () => {
     linkedList.append(0);
     linkedList.append(2);
     linkedList.append(3);
     linkedList.insertBefore(2, 1);
-    expect(linkedList.tostring()).toBe('0 >> 1 >> 2 >> 3 >> NULL');
+    expect(linkedList.tostring()).toBe("0 >> 1 >> 2 >> 3 >> NULL");
   });
 
-  it('should successfully insert after a node in the middle of the linked list', () => {
+  it("should successfully insert after a node in the middle of the linked list", () => {
     linkedList.append(1);
     linkedList.append(3);
     linkedList.insertAfter(1, 2);
-    expect(linkedList.tostring()).toBe('1 >> 2 >> 3 >> NULL');
+    expect(linkedList.tostring()).toBe("1 >> 2 >> 3 >> NULL");
   });
 
-  it('should successfully insert a node after the last node of the linked list', () => {
+  it("should successfully insert a node after the last node of the linked list", () => {
     linkedList.append(1);
     linkedList.insertAfter(1, 2);
-    expect(linkedList.tostring()).toBe('1 >> 2 >> NULL');
+    expect(linkedList.tostring()).toBe("1 >> 2 >> NULL");
   });
 
+  // it("returns null when k is greater than the length of the linked list", () => {
+  //   const linkedList = new LinkedList();
+  //   linkedList.append(1);
+  //   linkedList.append(2);
+  //   linkedList.append(3);
+  //   const result = linkedList.kthNodeFromTail(4);
+  //   expect(result).toBe('not found');
+  // });
+
+  // it("returns null when k and the length of the list are the same", () => {
+  //   const linkedList = new LinkedList();
+  //   linkedList.append(1);
+  //   linkedList.append(2);
+  //   linkedList.append(3);
+  //   const result = linkedList.kthNodeFromTail(3);
+  //   expect(result).toBe('not found');
+  // });
+
+  // it("throws an error when k is not a positive integer", () => {
+  //   const linkedList = new LinkedList();
+  //   linkedList.append(1);
+  //   linkedList.append(2);
+  //   linkedList.append(3);
+  //   expect(() => {
+  //     linkedList.kthNodeFromTail(-1);
+  //   }).toBe("k must be greater than 0 and less than or equal to the length of your linked list");
+  // });
+
+  // it("returns the value of the only node when the list size is 1", () => {
+  //   const linkedList = new LinkedList();
+  //   linkedList.append(1);
+  //   const result = linkedList.kthNodeFromTail(1);
+  //   expect(result).toBe(1);
+  // });
+
+  // it("returns the correct value when k is in the middle of the list", () => {
+  //   const linkedList = new LinkedList();
+  //   linkedList.append(1);
+  //   linkedList.append(2);
+  //   linkedList.append(3);
+  //   linkedList.append(4);
+  //   linkedList.append(5);
+  //   const result = linkedList.kthNodeFromTail(3);
+  //   expect(result).toBe(2);
+  // });
 });
