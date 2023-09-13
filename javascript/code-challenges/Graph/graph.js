8d9035c474a692e80ea43a745c3818eb59f06f0a
Nour-Alahmad <Nour.Alahmad1@outlook.com>
2023-09-13 11:07:24 +0300

    graph-business-trip

--- javascript/code-challenges/Graph/graph.js
+++ javascript/code-challenges/Graph/graph.js
@@ -69,4 +69,26 @@ class Graph {
     return result;
   }
 }
+//------------------------------------------------
+
+function businessTrip(graph, cities) {
+  if (!cities || cities.length < 2) {
+    return null;
+  }
+
+  let cost = 0;
+
+  for (let i = 0; i < cities.length - 1; i++) {
+    const currentCity = cities[i];
+    const nextCity = cities[i + 1];
+
+    if (!graph[currentCity] || !graph[currentCity][nextCity]) {
+      return null;
+    }
+
+    cost += graph[currentCity][nextCity];
+  }
+
+  return cost;
+}
 
